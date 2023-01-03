37ad50263806b4f4d434c3487f8e9af23cc3486a
Yulia-Chamlai <yuliachamlai@gmail.com>
2023-01-03 23:30:16 +0200

    upd

--- script.js
+++ script.js
@@ -7,9 +7,32 @@ function navToggle() {
   body.classList.toggle("_overflow");
 }
 
-$(".owl-carousel").owlCarousel({
-  items: 2,
+/*$(".owl-carousel").owlCarousel({
+  items: 2.5,
   lazyLoad: true,
   loop: true,
   margin: 10,
+});*/
+
+function carouselFn() {
+  if ($(window).width() < 500) {
+    $(".owl-carousel").owlCarousel({
+      items: 1,
+      margin: 10,
+      loop: true,
+    });
+  } else {
+    $(".owl-carousel").owlCarousel({
+      items: 2.5,
+      margin: 20,
+      loop: true,
+    });
+  }
+}
+
+$(document).ready(function () {
+  carouselFn();
+});
+$(window).resize(function () {
+  carouselFn();
 });
